2a380b730f3bc53c98f264c92f43eaa6ebc7d38b
Adam Szaruga <adamszaruga@gmail.com>
2018-06-28 16:55:21 -0400

    initial commit

---
+++ src/MemoryCard.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './MemoryCard.css'
+
+class Card extends Component {
+	render() {
+		return <div className="MemoryCard"><img src="https://www.digitalcrafts.com/img/DigitalCrafts-Logo-Wrench.png"/></div>
+	}
+}
+
+export default Card;
